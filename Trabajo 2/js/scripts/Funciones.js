5e6c53b683b197fa33044bb63689615df5517a0f
GabrielLapeire <lapeiregabriel@gmail.com>
2022-04-08 19:24:35 -0300

    fin 8/4/22

--- Trabajo 2/js/scripts/Funciones.js
+++ Trabajo 2/js/scripts/Funciones.js
@@ -1,9 +1,20 @@
 function saludar (params) {
-    alert ("hola mundo")
+    alert ("hola mundo");
 }
 function hola (params) {
-    alert ("hola")
+    alert ("hola");
 }
 function chau(params) {
-    alert ("chau")
+    alert ("chau");
+}
+function pasar(params) {
+    alert ("pasaste por aca");
+}
+function cambiar(params) {
+    document.getElementById("a1").style.color="red";
+    document.getElementById("a1").style.backgroundColor="black";
+}
+function original(params) {
+    document.getElementById("a1").style.color="black";
+    document.getElementById("a1").style.backgroundColor="blue";
 }
