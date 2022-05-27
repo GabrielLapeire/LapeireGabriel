6aaa4c14414c8d225d94b3ef99a5a99370837d88
lapeire gabriel <lapeiregabriel@gmail.com>
2022-05-27 19:18:56 -0300

    27/5

---
+++ Trabajo 9/formulario.php
@@ -0,0 +1,16 @@
+<?php
+$apellido = $_POST['apellido'];
+$nombre = $_POST['nombre'];
+$dni = $_POST['dni'];
+$fecha = $_POST['birth'];
+$sexo = $_POST['sexo'];
+$colores = $_POST['color'];
+
+echo $apellido ."<br>";
+echo $nombre ."<br>";
+echo $dni ."<br>";
+echo $fecha ."<br>";
+echo $sexo ."<br>";
+echo $colores ."<br>";
+//(isset())  array_push()
+?>
