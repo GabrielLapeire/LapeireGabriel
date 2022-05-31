f45961a2742e297a3c125db66d8ee14d9063a4ab
gabriel <lapeiregabriel@gmail.com>
2022-05-31 18:36:47 -0300

    31/5

--- Trabajo 9/formulario.php
+++ Trabajo 9/formulario.php
@@ -4,13 +4,16 @@ $nombre = $_POST['nombre'];
 $dni = $_POST['dni'];
 $fecha = $_POST['birth'];
 $sexo = $_POST['sexo'];
-$colores = $_POST['color'];
 
-echo $apellido ."<br>";
-echo $nombre ."<br>";
-echo $dni ."<br>";
-echo $fecha ."<br>";
-echo $sexo ."<br>";
-echo $colores ."<br>";
+echo "<strong>Apellido: </strong>".$apellido."<br>";
+echo "<strong>Nombre: </strong>".$nombre."<br>";
+echo "<strong>DNI: </strong>".$dni."<br>";
+echo "<strong>Fecha de nacimiento: </strong>".$fecha."<br>";
+echo "<strong>Sexo: </strong>".$sexo."<br>";
+echo "<strong>Colores: </strong><br>";
+if(!empty($_POST['color'])){
+    foreach($_POST['color'] as $aux){
+    echo $aux."</br>";
+    }}
 //(isset())  array_push()
 ?>
